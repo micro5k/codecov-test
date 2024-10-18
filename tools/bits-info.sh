684fd4822c442c0289fa7ed72396219cd3c56dec
ale5000 <15793015+ale5000-git@users.noreply.github.com>
2024-10-18 23:32:32 +0200

    Update bits-info.sh

--- tools/bits-info.sh
+++ tools/bits-info.sh
@@ -12,7 +12,9 @@ SCRIPT_VERSION='1.4'
 
 set -u 2> /dev/null || :
 # shellcheck disable=SC3040 # Ignore: In POSIX sh, set option pipefail is undefined
-case "$(set -o || :)" in *'pipefail'*) set -o pipefail || printf 1>&2 '%s\n' 'Failed: pipefail' ;; *) ;; esac
+case "$(set 2> /dev/null -o || :)" in *'pipefail'*) echo 'ok'; set -o pipefail || printf 1>&2 '%s\n' 'Failed: pipefail' ;; *) ;; esac
+set -o | grep pipefail
+set +o | grep pipefail
 
 # The "obosh" shell does NOT support "command" while the "posh" shell does NOT support "type"
 {
